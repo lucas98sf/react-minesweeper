99c1f744be0db06699b9cb12846249850162e584
Lucas98sf <lucas.98sf@gmail.com>
2021-09-03 23:16:13 -0300

    Get mouse button and prevent right click menu

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; //useState
+import React, { useEffect, useState } from "react";
 // import ReactDOM from "react-dom";
 import { Square, Value } from "../types";
 import { Bomb } from "./Bomb";
@@ -6,17 +6,29 @@ import { generateSquares, getSquareNumber } from "../utils";
 
 function Board() {
 	const [squares, setSquares] = useState(generateSquares());
+	document.addEventListener("contextmenu", (e) => e.preventDefault()); //dont show context menu
 
-	const handleClick = (r: number, c: number) => {
-		const newSquares: Square[][] = squares.slice();
-		const square: Square = newSquares[r][c];
+	useEffect(() => {});
 
-		if (!square.state.visible) {
-			square.state.value = square.hasBomb ? 9 : getSquareNumber(squares, r, c);
-			square.state.visible = true;
+	const handleClick = (e: React.MouseEvent<HTMLElement>, r: number, c: number) => {
+		if (e.button === 0) {
+			//left click
+			const newSquares: Square[][] = squares.slice();
+			const square: Square = newSquares[r][c];
+
+			if (!square.state.visible) {
+				square.state.value = square.hasBomb ? 9 : getSquareNumber(squares, r, c);
+				square.state.visible = true;
+			}
+
+			setSquares(newSquares);
 		}
-
-		setSquares(newSquares);
+		if (e.button === 1) {
+			//middle click
+		}
+		if (e.button === 2) {
+			//right click
+		}
 	};
 
 	const board = squares.map((rows, r) => {
@@ -24,8 +36,9 @@ function Board() {
 			const square: Square = squares[r][c];
 			return (
 				<button
-					className={square.state.visible ? Value[square.state.value] : "square"}
-					onClick={() => handleClick(r, c)}
+					className={square.state.visible ? `square ${Value[square.state.value]}` : "square"}
+					onClick={(e) => handleClick(e, r, c)}
+					onAuxClick={(e) => handleClick(e, r, c)}
 				>
 					{square.state.visible && square.hasBomb ? (
 						<Bomb />
