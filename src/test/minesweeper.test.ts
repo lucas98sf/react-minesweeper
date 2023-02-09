37f8fd65e0828a6d282c55e3a4a38f8b0024726d
lucas98sf <lucas.98sf@gmail.com>
2023-02-08 21:05:58 -0300

    test: add vitest and minesweeper logic tests

---
+++ src/test/minesweeper.test.ts
@@ -0,0 +1,122 @@
+import {
+  generateEmptySquares,
+  generateSquaresValues,
+  revealSquare,
+  revealSurroundingSquares,
+  toggleSquareFlag,
+  isGameLost,
+  isGameWon,
+} from '@/functions/minesweeper';
+import { describe, it, expect } from 'vitest';
+
+// all written by chatgpt btw
+
+describe('minesweeper logic', () => {
+  describe('generateEmptySquares', () => {
+    it('generates an empty board', () => {
+      const squares = generateEmptySquares();
+      expect(squares).toEqual(
+        expect.arrayContaining([
+          expect.arrayContaining([
+            expect.objectContaining({
+              hasBomb: false,
+              state: { flagged: false, visible: false, value: null },
+            }),
+          ]),
+        ]),
+      );
+    });
+  });
+
+  describe('generateSquaresValues', () => {
+    it('generates squares with the correct values', () => {
+      const firstClick = { row: 0, col: 0 };
+      const squares = generateSquaresValues(firstClick);
+      expect(squares).toEqual(
+        expect.arrayContaining([
+          expect.arrayContaining([
+            expect.objectContaining({
+              hasBomb: expect.any(Boolean),
+              state: expect.objectContaining({
+                flagged: false,
+                visible: expect.any(Boolean),
+                value: expect.any(Number),
+              }),
+            }),
+          ]),
+        ]),
+      );
+    });
+  });
+
+  describe('revealSquare', () => {
+    it('reveals the correct square', () => {
+      const firstClick = { row: 0, col: 0 };
+      const squares = generateSquaresValues(firstClick);
+      const clickCoords = { row: 0, col: 1 };
+      const newSquares = revealSquare(squares, clickCoords);
+      expect(newSquares[clickCoords.row][clickCoords.col].state.visible).toBe(true);
+    });
+  });
+
+  describe('revealSurroundingSquares', () => {
+    it('reveals the correct surrounding squares', () => {
+      const firstClick = { row: 0, col: 0 };
+      const squares = generateSquaresValues(firstClick);
+      const newSquares = revealSurroundingSquares(squares, firstClick);
+      expect(
+        newSquares.every(row => row.every(square => !(square.state.visible && square.hasBomb))),
+      ).toBe(true);
+    });
+  });
+
+  describe('toggleSquareFlag', () => {
+    it('toggles the flag on the correct square', () => {
+      const firstClick = { row: 0, col: 0 };
+      const squares = generateSquaresValues(firstClick);
+      const clickCoords = { row: 0, col: 1 };
+      const newSquares = toggleSquareFlag(squares, clickCoords);
+      expect(newSquares[clickCoords.row][clickCoords.col].state.flagged).toBe(true);
+    });
+  });
+
+  describe('isGameLost', () => {
+    it('returns true when a bomb is revealed', () => {
+      const squares = generateSquaresValues({ row: 0, col: 0 });
+      const bombIndex = squares.flat().findIndex(square => square.hasBomb);
+      const bombCoords = {
+        row: Math.floor(bombIndex / squares[0].length),
+        col: bombIndex % squares[0].length,
+      };
+      squares[bombCoords.row][bombCoords.col].state.visible = true;
+      const isLost = isGameLost(squares);
+      expect(isLost).toBe(true);
+    });
+  });
+
+  describe('isGameWon', () => {
+    it('returns true when all bombs are flagged or all squares are revealed', () => {
+      const squares = generateSquaresValues({ row: 0, col: 0 });
+      const bombSquares = squares.flat().filter(square => square.hasBomb);
+      const safeSquares = squares.flat().filter(square => !square.hasBomb);
+      // flag all bombs
+      bombSquares.forEach(bombSquare => {
+        // eslint-disable-next-line no-param-reassign
+        bombSquare.state.flagged = true;
+      });
+      // reveal all safe squares
+      safeSquares.forEach(safeSquare => {
+        // eslint-disable-next-line no-param-reassign
+        safeSquare.state.visible = true;
+      });
+      const isWon = isGameWon(squares);
+      expect(isWon).toBe(true);
+    });
+  });
+
+  describe('isBoardSolvable', () => {
+    it('should resolve an board with no guesses needed', () => {
+      // const squares = generateEmptySquares();
+    });
+  });
+});
